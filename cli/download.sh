9ac4166d74b30696449f1da49e260c79748cce66
Rafif <mirfanrafif17@gmail.com>
2024-01-31 18:34:17 +0700

    update translation

--- cli/download.sh
+++ cli/download.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-host='http://localhost:3001/api'
-project='pms'
-locales=('en' 'jp')
-directory='src/core/resources/locales'
+host=''
+project=''
+directory=''
+apiKey=''
+
+# get all locales
+locales=($(ls -d $directory/* | xargs -n 1 basename))
 
 for locale in "${locales[@]}"; do
   for file in "${directory}/${locale}"/*; do
     namespace=$(basename "${file}" .json)
-    data=$(curl -s "${host}/translations/export?project=${project}&namespace=${namespace}&locale=${locale}")
+    data=$(curl -s -H "Authorization: Api-Key ${apiKey}" "${host}/api/translations/export?project=${project}&namespace=${namespace}&locale=${locale}")
     echo "${data}" >"${file}"
   done
 done

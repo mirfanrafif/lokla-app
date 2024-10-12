9b1a63c9027c6c377311dbf5413734a86c8d18cb
Rafif <mirfanrafif17@gmail.com>
2024-10-12 18:16:50 +0700

    fix: add loading state

--- apps/lokla-app/app/utils/auth.ts
+++ apps/lokla-app/app/utils/auth.ts
@@ -16,19 +16,27 @@ export const authenticator = new Authenticator<ResponseLogin>(sessionStorage);
 // Tell the Authenticator to use the form strategy
 authenticator.use(
   new FormStrategy(async ({ form }) => {
-    const email = form.get('email');
-    const password = form.get('password');
-    const response = await getApiClient().post('/auth/login', {
-      email,
-      password,
-    });
+    try {
+      const email = form.get('email');
+      const password = form.get('password');
 
-    const data = ResponseLoginSchema.parse(response.data);
+      const response = await getApiClient().post('/auth/login', {
+        email,
+        password,
+      });
 
-    // the type of this user must match the type you pass to the Authenticator
-    // the strategy will automatically inherit the type if you instantiate
-    // directly inside the `use` method
-    return data;
+      console.log(response.data);
+
+      const data = ResponseLoginSchema.parse(response.data);
+
+      // the type of this user must match the type you pass to the Authenticator
+      // the strategy will automatically inherit the type if you instantiate
+      // directly inside the `use` method
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Invalid credentials');
+    }
   }),
   // each strategy has a name and can be changed to use another one
   // same strategy multiple times, especially useful for the OAuth2 strategy.
